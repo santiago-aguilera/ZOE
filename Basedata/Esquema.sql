18ef7e0fc170f850ddeac92be3fddbf292f7f9b9
ESTUDIANTE <santiago.aguilera681@hotmail.com>
2025-11-10 16:03:05 -0500

    esquema f

---
+++ Basedata/Esquema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE esquema (
+    id_esquema INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR (100),
+    Archivo VARCHAR (100),
+    ID_Usuario VARCHAR (100)
+);
+
+INSERT INTO Esquema(titulo,Archivo,)
+VALUES("Aprendizaje y Servicio","AP.MP4");
+
+INSERT INTO Esquema(titulo,Archivo,)
+VALUES("Proyecto de Reflexión","REFLEX.ZIP");
