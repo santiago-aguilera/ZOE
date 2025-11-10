d9d873bbd6330ece8c5447de70eff58890d1f630
Your Name <you@example.com>
2025-11-10 15:04:47 -0500

    Materias Basedata

---
+++ Basedata/Materias.sql
@@ -0,0 +1,30 @@
+CREATE TABLE materia
+(
+    id_materia int AUTO_INCREMENT PRIMARY KEY,
+    nombre_materia varchar(50),
+    Descripcion varchar(255)
+);
+
+INSERT INTO materia (Nombre_Materia, Descripcion)
+VALUES ('Lengua y Literatura', 'Materia donde se ve la literatura y se busca el reconocimiento y análisis de diferentes textos');
+
+INSERT INTO materia (Nombre_Materia, Descripcion)
+VALUES ('Matemáticas', 'Materia que cubre temas de álgebra, geometría, cálculo, probabilidad y estadística, entre otros');
+
+INSERT INTO materia (Nombre_Materia, Descripcion)
+VALUES ('Inglés', 'Estudio del idioma inglés, tanto en su aspecto gramatical como literario');
+
+INSERT INTO materia (Nombre_Materia, Descripcion)
+VALUES ('Aprendizaje y Servicio', 'Curso enfocado en la aplicación práctica de habilidades en la comunidad, combinando el aprendizaje académico con el servicio social');
+
+INSERT INTO materia (Nombre_Materia, Descripcion)
+VALUES ('Proyecto de Reflexión', 'Espacio de reflexión y evaluación personal sobre el aprendizaje y el desarrollo durante el Programa del IB');
+
+INSERT INTO materia (Nombre_Materia, Descripcion)
+VALUES ('Habilidades Personales y Profesionales (HPP)', 'Materia que desarrolla destrezas esenciales para la vida académica y laboral, como la gestión del tiempo, el trabajo en equipo y la resolución de problemas');
+
+INSERT INTO materia (Nombre_Materia, Descripcion)
+VALUES ('Desarrollo de Lenguas', 'Curso que promueve el aprendizaje de lenguas extranjeras y el desarrollo de competencias comunicativas en varios idiomas');
+
+INSERT INTO materia (Nombre_Materia, Descripcion)
+VALUES ('La Especialidad o Técnico', 'Área de enfoque práctico o técnico donde el estudiante desarrolla habilidades específicas relacionadas con su formación profesional o vocacional');
